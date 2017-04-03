ec52fd6e705f7a8833ae58ad04800ce4caf68cf5
GlisseLisbeth <glisselisbeth@gmail.com>
2017-04-03 11:17:05 -0500

    maquetacion

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,33 +1,17 @@
 var img = document.getElementsByClassName("animal");
 var select = document.getElementById("select");
-select.onchange = function(){ 
-    if(select.value == "original"){
-        for(var i =0; i<img.length;i++){
-            eliminar(img[i].classList);
-            img[i].classList.add("original");
-        }
-    }
-    if(select.value == "blanco-negro"){
-        for(var i =0; i<img.length;i++){
-            eliminar(img[i].classList);
-            img[i].classList.add("blanco-negro");
-        }
-    }
-    if(select.value == "sepia"){
-        for(var i =0; i<img.length;i++){
-            eliminar(img[i].classList);
-            img[i].classList.add("sepia");
-        }
-    }
-    if(select.value == "invertir-colores"){
-        for(var i =0; i<img.length;i++){
-            eliminar(img[i].classList);
-            img[i].classList.add("invertir-colores");
+var selector = ["original","blanco-negro","sepia","invertir-colores"];
+select.onchange = function(){
+  for (var i=0; i<selector.length; i++)
+    if(select.value == selector[i]){
+        for(var j =0; j<img.length;j++){
+            eliminar(img[j].classList);
+            img[j].classList.add(selector[i]);
         }
     }
 }
 
-function eliminar(param){        
+function eliminar(param){
    if(param.item(1) != ""){
        param.remove(param.item(1));
    }
